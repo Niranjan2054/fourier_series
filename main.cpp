12ff1e1f2d8d79a3f5fe14dc786c3fbbcb34c82a
Niranjan2054 <ckim6189@gmail.com>
2020-02-10 22:56:02 +0545

    Circle animated

--- main.cpp
+++ main.cpp
@@ -13,16 +13,26 @@ void init()
     glMatrixMode(GL_PROJECTION);
     glPointSize(3);
     // for setting the transformation which here is 2D
-    gluOrtho2D(0,600,-300,300);
+    gluOrtho2D(-400,1000,-300,300);
 }
 float mod_sine(int n,float x, float L)//subtract fomr 200/pi
 {
     float result =  400/((4*n*n-1)*pi)*cos(n*pi*x/L);
     return result;
 }
+float mod_sine1(int n,float x, float L)//subtract fomr 200/pi
+{
+    float result =  400/((4*n*n-1)*pi)*sin(n*pi*x/L);
+    return result;
+}
 float triangle(int n, float x, float L)//subtract from 1/2
 {
     float result =  400/(n*pi)*sin(n*pi*x/L);
+    return result;
+}
+float triangle1(int n, float x, float L)//subtract from 1/2
+{
+    float result =  400/(n*pi)*cos(n*pi*x/L);
     return result;
 }
 
@@ -31,6 +41,12 @@ float square(int n, float x, float L)
     float result =  400/((2*n-1)*pi)*sin((2*n-1)*pi*x/L);
     return result;
 }
+float square1(int n, float x, float L)
+{
+    float result =  400/((2*n-1)*pi)*cos((2*n-1)*pi*x/L);
+    return result;
+}
+
 float fourier_series(float (*f)(int,float,float),float x,float L,int no_of_iteration){
     float result=0;
     for(int i=1;i<=no_of_iteration;i++){
@@ -38,31 +54,72 @@ float fourier_series(float (*f)(int,float,float),float x,float L,int no_of_itera
     }
     return 1/2- result;
 }
+void drawCircle(float (*f)(int,float,float),float L,int n,int xc, int yc)
+{
+    int x,y;
+    glBegin(GL_POINTS);
+    for(float i = 0;i<2*L;i+=0.075){
+        y = f(n,i,L);
+        x = triangle1(n,i,L);
+//            printf("%f %f\n",x,y);
+        glVertex2f(x+xc,y+yc);
+    }
+    glEnd();
+}
 
+float drawFourierCircle(float (*f)(int,float,float),float L,int no_of_iteration,float x)
+{
+    int x_init=0,y_init=0;
+    int xc, yc;
+    for(int n=1;n<=no_of_iteration+1;n++){
+        yc = fourier_series(f,x,L,n-1);
+        xc = fourier_series(triangle1,x,L,n-1);
+        if(n<=no_of_iteration){
+            drawCircle(f,L,n,xc,yc);
+        }
+        if(n!=1){
+            glBegin(GL_LINES);
+                glVertex2f(x_init,y_init);
+                glVertex2f(xc,yc);
+            glEnd();
+        }
+        x_init = xc;
+        y_init = yc;
+    }
+    glBegin(GL_LINES);
+        glVertex2f(x_init,y_init);
+        glVertex2f(x+300,y_init);
+    glEnd();
 
-
+}
 void display()
 {
     float x;
     float tempi=0,tempx=0;
-    for(float i = 0;i<600;i+=1){
+    int iteration = 20;
+    int offset= 300;
+    int time_period=120;
+    for(float i = 0;i<600;i+=0.9){
         glClear(GL_COLOR_BUFFER_BIT); // Clear display window
         glBegin(GL_LINES);
-            glVertex2f(0,0);
-            glVertex2f(600,0);
+            glVertex2f(0+offset,0);
+            glVertex2f(600+offset,0);
         glEnd();
-        for(float j = 0;j<i;j+=1)
+        for(float j = 0;j<i;j+=0.02)
         {
-            x= fourier_series(triangle,j,120,4);
+
+            x= fourier_series(triangle,j,time_period,iteration);
+
             if(j!=0){
                 glBegin(GL_LINES);
-                    glVertex2f(tempi,tempx);
-                    glVertex2f(j,x);
+                    glVertex2f(tempi+offset,tempx);
+                    glVertex2f(j+offset,x);
                 glEnd();
             }
             tempi= j;tempx=x;
-            printf("i = %f, x = %f\n",j,x);
         }
+        drawFourierCircle(triangle,time_period,iteration,i);
+        printf("i = %f, x = %f\n",i,x);
         glFlush();
     }
 
@@ -77,7 +134,7 @@ int main(int agrc, char ** argv)
     // Set top - left display window position.
     glutInitWindowPosition(100, 100);
     // Set display window width and height
-    glutInitWindowSize(600, 600);
+    glutInitWindowSize(1400, 600);
     // Create display window with the given title
     glutCreateWindow("Fourier Series");
     // Execute initialization procedure
